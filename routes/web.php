b5f0e65cddf54b2177a0a0e087c9b316c7421a19
Xiaochen Yang <bobyang@Xiaochens-MacBook-Pro.local>
2020-02-08 23:54:05 -0800

    first

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/invoices', 'InvoiceController@index');
+
+Route::get('/invoices/{id}', 'InvoiceController@show');
